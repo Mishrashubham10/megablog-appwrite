2349898199ab773734f63cb2bb3b7ce03b5859db
Shubam Mishra <shubhammishra10101998@gmail.com>
2024-04-29 21:15:09 +0530

    Tailwindcss and .env configuration

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,12 @@
 const App = () => {
+  console.log(import.meta.env.VITE_APP_APPWRITE_URL);
+
   return (
-    <div>App</div>
+    <div>
+      <h1 className="text-6xl text-green-300">
+        Appwirte Megablog with Vite.
+      </h1>
+    </div>
   )
 }
 
