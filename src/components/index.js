f6bdda721c3b6ea2ab28b2d0467023bd9e84be4d
Shubam Mishra <shubhammishra10101998@gmail.com>
2024-05-01 17:34:23 +0530

    Creates posts pages

--- src/components/index.js
+++ src/components/index.js
@@ -5,5 +5,9 @@ import Logo from './Logo';
 import LogoutBtn from './header/LogoutBtn';
 import Button from './Button';
 import RTE from './RTE';
+import Register from './Register';
+import Login from './Login';
+import PostCard from './PostCard'
+import PostForm from './post-form/PostForm'
 
-export { Header, Footer, Container, Logo, LogoutBtn, Button, RTE };
+export { Header, Footer, Container, Logo, LogoutBtn, Button, RTE, Register, Login, PostCard, PostForm };
---
+++ src/pages/Signup.jsx
@@ -0,0 +1,11 @@
+import { Register } from '../components';
+
+const Signup = () => {
+  return (
+    <div className="py-8">
+      <Register />
+    </div>
+  );
+};
+
+export default Signup;
